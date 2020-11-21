82bf0e32d25c9ab4c3a248f5dee119d8218cc7bf
RostislavBartenev <rostislav.bartenev@gmail.com>
2020-11-21 16:40:58 +0600

    add logs

--- main.js
+++ main.js
@@ -1,63 +1,108 @@
+const $getElById = (id) => document.getElementById(id)
+
 const $controlButtons = document.querySelectorAll('button')
 
 const character = {
   name: 'Pikachu',
-  defaultHP: 100,
-  damageHP: 100,
-  elHP: document.getElementById('health-character'),
-  elProgressbar: document.getElementById('progressbar-character'),
-  attack: [20, 80]
+  type: 'electric',
+  weakness: ['fire', 'water'],
+  resistance: ['steel'],
+  hp: {
+    current: 100,
+    total: 100,
+  },
+  attack: [20, 50],
+  elHP: $getElById('health-character'),
+  elProgressbar: $getElById('progressbar-character'),
+  changeHP: changeHP,
+  renderHP: renderHP,
+  renderHPLife: renderHPLife,
+  renderProgressbarHP: renderProgressbarHP
 }
 
 const enemy = {
   name: 'Charmander',
-  defaultHP: 100,
-  damageHP: 100,
-  elHP: document.getElementById('health-enemy'),
-  elProgressbar: document.getElementById('progressbar-enemy'),
+  type: 'fire',
+  weakness: ['water'],
+  resistance: ['electric'],
+  hp: {
+    current: 100,
+    total: 100,
+  },
+  attack: [30, 40],
+  elHP: $getElById('health-enemy'),
+  elProgressbar: $getElById('progressbar-enemy'),
+  changeHP: changeHP,
+  renderHP: renderHP,
+  renderHPLife: renderHPLife,
+  renderProgressbarHP: renderProgressbarHP
 }
 
 const random = (num) => {
   return Math.ceil(Math.random() * num)
 }
 
-const renderHPLife = (person) => person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP
-
-const renderProgressbarHP = (person) => {
-  person.elProgressbar.style.width = person.damageHP + '%'
+function renderHPLife() {
+  this.elHP.innerText = this.hp.current + ' / ' + this.hp.total
 }
 
-const renderHP = (person) => {
-  renderHPLife(person)
-  renderProgressbarHP(person)
+
+function renderProgressbarHP() {
+  this.elProgressbar.style.width = this.hp.current + '%'
 }
 
-const changeHP = (count, person) => {
-  if (person.damageHP <= count) {
-    person.damageHP = 0
-    alert('Бедный ' + person.name + ' проиграл бой!')
+function renderHP() {
+  this.renderHPLife()
+  this.renderProgressbarHP()
+}
+
+function changeHP(count) {
+  this.hp.current -= count
+
+  const log = enemy ? generateLog(this, character, count) : generateLog(this, enemy, count)
+
+  const logText = `<p>${log}</p>`
+  $getElById('logs').insertAdjacentHTML('afterbegin', logText)
+  console.log(logText)
+
+  if (this.hp.current <= 0) {
+    this.hp.current = 0
+    alert('Бедный ' + this.name + ' проиграл бой!')
     for (let i = 0; i < $controlButtons.length; i++) {
       $controlButtons[i].disabled = true
     }
-  } else {
-    person.damageHP -= count
   }
 
-  renderHP(person)
+  this.renderHP()
 }
+
 
 for (let i = 0; i < $controlButtons.length; i++) {
   $controlButtons[i].addEventListener('click', () => {
-    changeHP(random(character.attack[i]), enemy)
+    enemy.changeHP(random(character.attack[i]))
   })
 }
 
+function init() {
+  character.renderHP()
+  enemy.renderHP()
+}
 
+  function generateLog({name, hp}, {name: enemyName}, damage) {
+  const logs = [
+    `${name} вспомнил что-то важное, но неожиданно ${enemyName} , не помня себя от испуга, ударил в предплечье врага.`,
+    `${name} поперхнулся, и за это ${enemyName} с испугу приложил прямой удар коленом в лоб врага.`,
+    `${name} забылся, но в это время наглый ${enemyName}, приняв волевое решение, неслышно подойдя сзади, ударил.`,
+    `${name} пришел в себя, но неожиданно ${enemyName} случайно нанес мощнейший удар.`,
+    `${name} поперхнулся, но в это время ${enemyName} нехотя раздробил кулаком \<вырезанно цензурой\> противника.`,
+    `${name} удивился, а ${enemyName} пошатнувшись влепил подлый удар.`,
+    `${name} высморкался, но неожиданно ${enemyName} провел дробящий удар.`,
+    `${name} пошатнулся, и внезапно наглый ${enemyName} беспричинно ударил в ногу противника`,
+    `${name} расстроился, как вдруг, неожиданно ${enemyName} случайно влепил стопой в живот соперника.`,
+    `${name} пытался что-то сказать, но вдруг, неожиданно ${enemyName} со скуки, разбил бровь сопернику.`
+  ];
 
-const init = () => {
-  console.log('Start Game')
-  renderHP(character)
-  renderHP(enemy)
+  return logs[random(logs.length) - 1] + ` -${damage} [${hp.current <= 0 ? '0' : hp.current} / ${hp.total}]`
 }
 
 init()

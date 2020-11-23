dbc11249de5c8712ec9de882ee511c11a231cad9
RostislavBartenev <rostislav.bartenev@gmail.com>
2020-11-23 16:33:08 +0600

    add count

--- main.js
+++ main.js
@@ -11,13 +11,22 @@ const character = {
     current: 100,
     total: 100,
   },
-  attack: [20, 50],
+  attack: {
+    0: {
+      count: 6,
+      damage: 30
+    },
+    1: {
+      count: 3,
+      damage: 50
+    },
+  },
   elHP: $getElById('health-character'),
   elProgressbar: $getElById('progressbar-character'),
-  changeHP: changeHP,
-  renderHP: renderHP,
-  renderHPLife: renderHPLife,
-  renderProgressbarHP: renderProgressbarHP
+  changeHP,
+  renderHP,
+  renderHPLife,
+  renderProgressbarHP,
 }
 
 const enemy = {
@@ -29,13 +38,22 @@ const enemy = {
     current: 100,
     total: 100,
   },
-  attack: [30, 40],
+  attack: {
+    0: {
+      count: 6,
+      damage: 30
+    },
+    1: {
+      count: 3,
+      damage: 50
+    },
+  },
   elHP: $getElById('health-enemy'),
   elProgressbar: $getElById('progressbar-enemy'),
-  changeHP: changeHP,
-  renderHP: renderHP,
-  renderHPLife: renderHPLife,
-  renderProgressbarHP: renderProgressbarHP
+  changeHP,
+  renderHP,
+  renderHPLife,
+  renderProgressbarHP,
 }
 
 const random = (num) => {
@@ -59,11 +77,10 @@ function renderHP() {
 function changeHP(count) {
   this.hp.current -= count
 
-  const log = enemy ? generateLog(this, character, count) : generateLog(this, enemy, count)
+  const log = this === enemy ? generateLog(this, character, count) : generateLog(this, enemy, count)
 
   const logText = `<p>${log}</p>`
   $getElById('logs').insertAdjacentHTML('afterbegin', logText)
-  console.log(logText)
 
   if (this.hp.current <= 0) {
     this.hp.current = 0
@@ -79,7 +96,19 @@ function changeHP(count) {
 
 for (let i = 0; i < $controlButtons.length; i++) {
   $controlButtons[i].addEventListener('click', () => {
-    enemy.changeHP(random(character.attack[i]))
+
+
+
+    if (character.attack[i].count <= 0) return $controlButtons[i].disabled = true
+
+    enemy.changeHP(random(character.attack[i].damage))
+    character.changeHP(random(enemy.attack[i].damage))
+
+    return function counter() {
+      character.attack[i].count--
+      const log = `<p>У удара ${$controlButtons[i].innerText} осталось ${character.attack[i].count}</p>`
+      $getElById('logs').insertAdjacentHTML('afterbegin', log)
+    }()
   })
 }
 
@@ -88,18 +117,19 @@ function init() {
   enemy.renderHP()
 }
 
-  function generateLog({name, hp}, {name: enemyName}, damage) {
+  function generateLog({ name: characterName, hp }, { name: enemyName }, damage) {
+
   const logs = [
-    `${name} вспомнил что-то важное, но неожиданно ${enemyName} , не помня себя от испуга, ударил в предплечье врага.`,
-    `${name} поперхнулся, и за это ${enemyName} с испугу приложил прямой удар коленом в лоб врага.`,
-    `${name} забылся, но в это время наглый ${enemyName}, приняв волевое решение, неслышно подойдя сзади, ударил.`,
-    `${name} пришел в себя, но неожиданно ${enemyName} случайно нанес мощнейший удар.`,
-    `${name} поперхнулся, но в это время ${enemyName} нехотя раздробил кулаком \<вырезанно цензурой\> противника.`,
-    `${name} удивился, а ${enemyName} пошатнувшись влепил подлый удар.`,
-    `${name} высморкался, но неожиданно ${enemyName} провел дробящий удар.`,
-    `${name} пошатнулся, и внезапно наглый ${enemyName} беспричинно ударил в ногу противника`,
-    `${name} расстроился, как вдруг, неожиданно ${enemyName} случайно влепил стопой в живот соперника.`,
-    `${name} пытался что-то сказать, но вдруг, неожиданно ${enemyName} со скуки, разбил бровь сопернику.`
+    `${characterName} вспомнил что-то важное, но неожиданно ${enemyName} , не помня себя от испуга, ударил в предплечье врага.`,
+    `${characterName} поперхнулся, и за это ${enemyName} с испугу приложил прямой удар коленом в лоб врага.`,
+    `${characterName} забылся, но в это время наглый ${enemyName}, приняв волевое решение, неслышно подойдя сзади, ударил.`,
+    `${characterName} пришел в себя, но неожиданно ${enemyName} случайно нанес мощнейший удар.`,
+    `${characterName} поперхнулся, но в это время ${enemyName} нехотя раздробил кулаком \<вырезанно цензурой\> противника.`,
+    `${characterName} удивился, а ${enemyName} пошатнувшись влепил подлый удар.`,
+    `${characterName} высморкался, но неожиданно ${enemyName} провел дробящий удар.`,
+    `${characterName} пошатнулся, и внезапно наглый ${enemyName} беспричинно ударил в ногу противника`,
+    `${characterName} расстроился, как вдруг, неожиданно ${enemyName} случайно влепил стопой в живот соперника.`,
+    `${characterName} пытался что-то сказать, но вдруг, неожиданно ${enemyName} со скуки, разбил бровь сопернику.`
   ];
 
   return logs[random(logs.length) - 1] + ` -${damage} [${hp.current <= 0 ? '0' : hp.current} / ${hp.total}]`
